edba6267eedf56641f99c9ac1c8f80d3cf2dc9a8
mr-Delusion <franciskmachua@gmail.com>
2024-11-24 19:01:16 +0300

    Joining 2

--- database.py
+++ database.py
@@ -8,3 +8,38 @@ print("Connection Succesful!")
 
 import pandas as pd
 
+tables = pd.read_sql("""
+SELECT * FROM sqlite_master WHERE type = "table";
+""", conn)
+
+joined_cities = pd.read_sql("""
+SELECT Country. Country_id, Country.Country_Name, City. City_Name
+FROM Country
+INNER JOIN City
+ON Country. Country_Id == City. Country_Id
+""")
+print(tables)
+
+print(joined_cities)
+
+joined_left = pd.read_sql("""
+SELECT * FROM Player
+LEFT JOIN season
+ON player. Player_Id == season.Man_of_the_Series
+""", conn)
+
+print(joined_left)
+
+joined_cross = pd.read-sql("""
+SELECT c.Country_id, c.Country_Name, ci.City_Name
+FROM Country c
+CROSS JOIN city ci
+""")
+
+union = pd.read_sql("""
+SELECT Player_Name FROM Player
+UNION
+SELECT Team_Name FROM team
+""", conn)
+
+print(union)
